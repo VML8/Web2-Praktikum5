ec1b1baa116cc4bdbf8452144fc2c386a6d138b9
VML8 <92667459+VML8@users.noreply.github.com>
2022-04-24 22:03:48 +0700

    Initial commit

---
+++ data_dispenser.php
@@ -0,0 +1,16 @@
+<?php
+require_once "class_dispenser.php";
+  
+  $susu = new Dispenser(12, 500, 'kopi', 6);
+  $susu->what();
+  $susu->refill(880);
+  $susu->buy(2);
+  $susu->recharge(2500);
+  $susu->buy(2);
+  $susu->buy(5);
+  $susu->recharge(3000);
+  $susu->buy(5);
+  $susu->refillCup(4);
+  $susu->refill(660);
+  $susu->buy(5);
+?>
